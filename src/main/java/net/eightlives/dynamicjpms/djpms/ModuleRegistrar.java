a83a4e1b6f467d627eba9a0207d590eb28bd025a
Zack Brown <zackrbrown@gmail.com>
2018-05-05 22:59:34 -0500

    added javadoc

--- src/main/java/net/eightlives/dynamicjpms/djpms/ModuleRegistrar.java
+++ src/main/java/net/eightlives/dynamicjpms/djpms/ModuleRegistrar.java
@@ -14,6 +14,16 @@ public interface ModuleRegistrar {
         return new ModuleRegistrarImpl(new ModuleNodeResolverImpl(), ForkJoinPool.commonPool());
     }
 
+    /**
+     * Registers a JPMS module with DJPMS. It will be resolved once all of its dependencies are resolved. These dependencies
+     * can either exist in the module boot layer or in DJPMS. DJPMS dependencies can either already be registered and resolved,
+     * or be registered and resolved in the future. Until a registered module is resolved, it is not usable (i.e. a ModuleLayer
+     * is not created).
+     *
+     * @param moduleName the name of the module (located in the module's <code>module-info.java</code>)
+     * @param moduleLocation the location on the file system of the module, according to the lookup rules defined by JPMS
+     * @return a future that will contain the {@link ModuleLayer} of the registered module once it is resolved
+     */
     CompletableFuture<ModuleLayer> registerModule(String moduleName, Path moduleLocation);
 
     void unregisterModule(String moduleName);
